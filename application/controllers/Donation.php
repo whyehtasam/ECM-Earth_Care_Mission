d37a75bf089d8b0c39d9c5c67a6d053f63313af7
whyehtasam <ehtasam2003197@st.jmi.ac.in>
2023-11-01 13:53:29 +0530

    copied files from local device to the remote

---
+++ application/controllers/Donation.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Donation extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('Donate');
+	}
+	
+	public function detailsConfirmation(){
+		$data['DonationType'] = $this->input->post('frmDonationType');
+		$data['Name'] = $this->input->post('name');
+		$data['Mobile'] = $this->input->post('mobile');
+		$data['Email'] = $this->input->post('email');
+		$data['Amount'] = $this->input->post('amount');
+
+		$this->load->view('PageConfirm',$data);
+
+	}
+
+}
